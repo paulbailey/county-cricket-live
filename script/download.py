b1f636386f84670d2289182613fe3e5d0a44ad59
Paul Bailey <bailey@dreamshake.net>
2023-05-04 10:55:26 -0400

    Use actual API

--- script/download.py
+++ script/download.py
@@ -1,5 +1,9 @@
 import os
 
+from googleapiclient.discovery import build
+
 if "GOOGLE_API_KEY" in os.environ and os.environ["GOOGLE_API_KEY"] != "":
     print("API key present")
+    youtube_service = build("youtube", "v3",developerKey=os.environ["GOOGLE_API_KEY"])
 print("Hello world")
+
