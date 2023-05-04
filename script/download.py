1f7deb4f8e89a1d0cf1b4fdc2de325acff65d2c4
Paul Bailey <bailey@dreamshake.net>
2023-05-04 11:09:11 -0400

    Wrong name!

--- script/download.py
+++ script/download.py
@@ -3,7 +3,7 @@ import os
 
 from googleapiclient.discovery import build
 
-with open("../counties.json") as json_counties:
+with open("channels.json") as json_counties:
     counties = json.load(json_counties)
     youtube_service = build("youtube", "v3",developerKey=os.environ["GOOGLE_API_KEY"])
     for county in counties:
