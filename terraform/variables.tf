c91cf05f7a8d941f69547a5f6c9b7c9e1c378136
Paul Bailey <bailey@dreamshake.net>
2025-04-05 19:01:25 +0100

    Update scores every 2 minutes instead

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,11 +7,11 @@ variable "github_token" {
 variable "poll_youtube_schedule" {
   description = "EventBridge scheduler cron expression for poll-youtube workflow"
   type        = string
-  default     = "cron(0/5 8-20 * * ? *)" # Every 5 minutes between 8 AM and 8:59 PM UTC
+  default     = "cron(0/5 8-21 * * ? *)" # Every 5 minutes between 8 AM and 8:59 PM UTC
 }
 
 variable "deploy_schedule" {
   description = "EventBridge scheduler cron expression for deploy workflow"
   type        = string
-  default     = "cron(* 8-20 * * ? *)" # Every minute between 8 AM and 8:59 PM UTC
+  default     = "cron(0/2 8-21 * * ? *)" # Every 2 minutes between 8 AM and 8:59 PM UTC
 }
